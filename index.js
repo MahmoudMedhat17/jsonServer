5b4afcdd1e2e5dee6cbe366ea26188e0fd0f6de9
Mahmoud Medhat <M7moooud.17@gmail.com>
2024-05-23 20:55:37 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
+server.use(cors());
 
 server.listen(port);
